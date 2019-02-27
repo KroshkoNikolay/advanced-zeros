ddf66aaf33e718530b6b10c1ba8b6ec2d3d655d2
KroshkoNikolay <kolia_kroshko@ukr.net>
2019-02-27 20:46:54 +0200

    feat: add solution to calculate factorial trailing zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  if (number < 1 || number != Math.floor(number)) {
+    return {error: 'Wrong income data! Number must be positive integer'}
+  }
+
+  if (base < 1 || base != Math.floor(base)) {
+    return {error: 'Wrong income data! Base must be positive integer'}
+  }
+
+  const splittedBase = splitBase(base);
+  const times = countTime(number, splittedBase)
+  return Math.min(...times);
+}
+
+function splitBase(base){
+  const resp = {}
+  let temp = base;
+  let divider = 2;
+  do {
+    if (temp % divider == 0) {
+      resp[divider] = (resp[divider] || 0) + 1;
+      temp /= divider;
+      divider = 2;
+    } else {
+      divider++;
+    }
+  }
+  while(temp != 1)
+  return resp
+}
+
+function countTime(number, split){
+  const resp = [];
+  for (const key in split) {
+    let times = 0;
+    let degree = 1
+    do {
+      divider = Math.pow(key, degree);
+      if (divider > number)
+        break;
+      times += Math.floor(number / divider);
+      degree++;
+    }
+    while(true)
+    resp.push(Math.floor(times / split[key]))
+  }
+  return resp
 }
